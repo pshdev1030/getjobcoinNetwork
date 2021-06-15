e75ff15aa7d620d368358385bd7cc39f99d25825
DongHyeon Park <paramore@kakao.com>
2021-06-15 16:46:39 +0900

    코드 정리

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -28,20 +28,17 @@ public class Server {
             remotePort = Integer.parseInt(args[2]);
         }
         catch(NumberFormatException e){ return;}
-
         System.out.println("Start on " + localPort);
         BlockChain blockchain = new BlockChain();
-
         HashMap<Peer, Date> serverStatus = new HashMap<Peer, Date>();
         serverStatus.put(new Peer(remoteHost, remotePort), new Date());
 
-        PublishRunnable pcr = new PublishRunnable(blockchain);
-        Thread pct = new Thread(pcr);
-        pct.start();
-
+        //주기적으로 트랜잭션 목록을 확인하고 블록 생성
+        PublishRunnable pRunnable = new PublishRunnable(blockchain);
+        Thread pThread = new Thread(pRunnable);
+        pThread.start();
         //주기적으로 Heartbeat msg를 보냄(모든 노드에게)
         new Thread(new HeartBeatRunnable(serverStatus, localPort)).start();
-
         //주기적으로 다른 노드들에게 블록체인 데이터 동기화
         new Thread(new CatchupRunnable(blockchain, serverStatus, localPort)).start();
 
@@ -53,17 +50,19 @@ public class Server {
                 new Thread(new BlockChainServerRunnable(clientSocket, blockchain, serverStatus, localPort)).start(); //본 서버와 연결된 소켓에서 오는 메시지 및 명령을 처리하기 위한 스레드
                 new Thread(new HeartBeatReceiverRunnable(clientSocket, serverStatus, localPort)).start(); // HeartBeat MSG 전문 수신 스레드
             }
-        } catch (IllegalArgumentException e) {
-        } catch (IOException e) {
-        } finally {
+        }
+        catch (IllegalArgumentException e) { }
+        catch (IOException e) { }
+        finally {
             try {
-                pcr.setRunning(false);
-                pct.join();
-                if (serverSocket != null)
+                pRunnable.setRunning(false);
+                pThread.join();
+                if (serverSocket != null){
                     serverSocket.close();
-            } catch (IOException e) {
-            } catch (InterruptedException e) {
+                }
             }
+            catch (IOException e) { }
+            catch (InterruptedException e) { }
         }
     }
 }
