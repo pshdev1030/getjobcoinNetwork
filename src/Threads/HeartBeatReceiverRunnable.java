6191739210c0f2fdcfe31ef24139b1c866177597
DongHyeon Park <paramore@kakao.com>
2021-06-16 14:00:11 +0900

    Update HeartBeatReceiverRunnable.java

--- src/Threads/HeartBeatReceiverRunnable.java
+++ src/Threads/HeartBeatReceiverRunnable.java
@@ -15,12 +15,12 @@ import java.util.HashMap;
 public class HeartBeatReceiverRunnable implements Runnable{
 
     private Socket toClient;
-    private HashMap<Peer, Date> serverStatus;
+    private HashMap<Peer, Date> peerList;
     private int localPort;
 
-    public HeartBeatReceiverRunnable (Socket toClient, HashMap<Peer, Date> serverStatus, int localPort) {
+    public HeartBeatReceiverRunnable (Socket toClient, HashMap<Peer, Date> peerList, int localPort) {
         this.toClient = toClient;
-        this.serverStatus = serverStatus;
+        this.peerList = peerList;
         this.localPort = localPort;
     }
 
@@ -52,19 +52,19 @@ public class HeartBeatReceiverRunnable implements Runnable{
                 	case "HB":
                 		serverInQuestion = new Peer(remoteIP, Integer.parseInt(tokens[1]));
                 		
-                		if (!serverStatus.containsKey(serverInQuestion)) {
+                		if (!peerList.containsKey(serverInQuestion)) {
                 			String forwardMessage = "SI|" + localPort + "|" + remoteIP + "|" + tokens[1];
                     		this.broadcast(forwardMessage, new ArrayList<Peer>());
                 		}
                 		
-                		serverStatus.put(serverInQuestion, new Date());
+                		peerList.put(serverInQuestion, new Date());
                 		this.removeUnresponsive();
             			
                 	case "SI":
                 		serverInQuestion = new Peer(tokens[2], Integer.parseInt(tokens[3]));
                 		Peer originator = new Peer(remoteIP, Integer.parseInt(tokens[1]));
                 		
-                		if (!serverStatus.containsKey(serverInQuestion)) {
+                		if (!peerList.containsKey(serverInQuestion)) {
                     		ArrayList<Peer> exempt = new ArrayList<Peer>();
                     		exempt.add(originator);
                     		exempt.add(serverInQuestion);
@@ -72,8 +72,8 @@ public class HeartBeatReceiverRunnable implements Runnable{
                     		this.broadcast(relayMessage, exempt);
                 		}
                 		
-                		serverStatus.put(serverInQuestion, new Date());
-                		serverStatus.put(originator, new Date());
+                		peerList.put(serverInQuestion, new Date());
+                		peerList.put(originator, new Date());
                 		this.removeUnresponsive();
                     	
                 	default:
@@ -85,9 +85,9 @@ public class HeartBeatReceiverRunnable implements Runnable{
     
     public void removeUnresponsive() {
     	//check for servers that havent responded in 4 secs
-        for (Peer server: serverStatus.keySet()) {
-            if (new Date().getTime() - serverStatus.get(server).getTime() > 4000) {
-            	serverStatus.remove(server);
+        for (Peer server: peerList.keySet()) {
+            if (new Date().getTime() - peerList.get(server).getTime() > 4000) {
+            	peerList.remove(server);
             	System.out.println("removed " + server.getHost());
             }
         }
@@ -95,7 +95,7 @@ public class HeartBeatReceiverRunnable implements Runnable{
     
     public void broadcast(String message, ArrayList<Peer> exempt) {
     	ArrayList<Thread> threadArrayList = new ArrayList<Thread>();
-    	for (Peer info: this.serverStatus.keySet()) {
+    	for (Peer info: this.peerList.keySet()) {
             if (!exempt.contains(info)) {
                 Thread thread = new Thread(new MessageSenderRunnable(info, message));
                 thread.start();
